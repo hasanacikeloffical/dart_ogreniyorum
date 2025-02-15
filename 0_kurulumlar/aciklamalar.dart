1099ed278d18eb8508ce2bbf77a4815a05ac1d74
hasan <hsnckel19@gmail.com>
2025-02-15 19:35:09 +0300

    kurulum ve ilk program

---
+++ 0_kurulumlar/aciklamalar.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print("hasan açıkel");
+  print("hasan'ın dersi");
+  print("3*5");
+  print(3 * 5);
+  print("3*5= ${3 * 5}");
+  print("hasan" + " açıkel");
+  //bu işaret yorum satırlarıdır.
+
+} 
